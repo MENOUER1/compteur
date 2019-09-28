7e3008b0d7a0a985ba1cec211956fd33a8446b14
MENOUER1 <menouerr2@gmail.com>
2019-09-28 22:30:52 +0200

    array

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 "use strict"
 
 const h1 = document.querySelector("h1");
-const Player1Button = document.querySelectorAll("button")
-const player2Button = document.querySelectorAll("button")
+const Player1Button = document.querySelectorAll("button")[0];
+const player2Button = document.querySelectorAll("button")[1];
