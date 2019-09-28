0aef0481f6db7a759f126094824d565c458e326d
MENOUER1 <menouerr2@gmail.com>
2019-09-28 22:56:44 +0200

    update

--- script.js
+++ script.js
@@ -3,3 +3,4 @@
 const h1 = document.querySelector("h1");
 const Player1Button = document.querySelectorAll("button")[0];
 const player2Button = document.querySelectorAll("button")[1];
+const input = document.querySelector("input");
