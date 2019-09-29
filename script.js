ebaad9c0e9edb8d1de6d04635e4674fc3ef20127
MENOUER1 <menouerr2@gmail.com>
2019-09-29 20:25:17 +0200

    update

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 "use strict"
 
 const h1 = document.querySelector("h1");
-const Player1Button = document.querySelectorAll("button")[0];
-const player2Button = document.querySelectorAll("button")[1];
+const reset = document.querySelectorAll("button")[0];
+const FirstButton =  document.querySelectorAll("button")[1]
+const Second2Button = document.querySelectorAll("button")[2];
 const input = document.querySelector("input");
+const scoreFirst= document.querySelectorAll("span")[0]
+const scoreSecond = document.querySelectorAll("span")[1];
