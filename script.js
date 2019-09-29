dca32a62e8e4164baa4002930d1e9caa23a20e74
MENOUER1 <menouerr2@gmail.com>
2019-09-29 22:05:05 +0200

    update

--- script.js
+++ script.js
@@ -5,5 +5,24 @@ const reset = document.querySelectorAll("button")[0];
 const FirstButton =  document.querySelectorAll("button")[1]
 const Second2Button = document.querySelectorAll("button")[2];
 const input = document.querySelector("input");
-const scoreFirst= document.querySelectorAll("span")[0]
-const scoreSecond = document.querySelectorAll("span")[1];
+const endScore = document.querySelectorAll("span")[0];
+const scoreFirst= document.querySelectorAll("span")[1]
+const scoreSecond = document.querySelectorAll("span")[2];
+let scoreOne = 0;
+let scoreTwo = 0;
+let scoreToWin = 5;
+// creation des constantes et variables
+
+function addScoreForScoreOne() {
+	
+	if ( scoreOne< scoreMax && scoreFirst != scoreToWin) {
+		
+        scoreOne ++;
+    }
+
+function addScoreForScoreTwo() {
+    
+    if  ( scoreTwo< scoreMax && scoreFirst != scoreToWin) {
+		
+        scoreTwo ++;
+}
